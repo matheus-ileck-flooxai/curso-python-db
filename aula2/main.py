9fae66beb3c3d03aa1cb5e785be02c38d399caa6
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-07-07 10:17:33 -0300

    refactor: use environment variables for DB connection

--- aula2/main.py
+++ aula2/main.py
@@ -1,11 +1,18 @@
 import pymysql
+import dotenv
+import os
+
+
+
+dotenv.load_dotenv()
 
 connection = pymysql.connect(
-    host='localhost',
-    user='usuario',
-    password='senha',
-    database='BD_ENV',
+    host=os.environ['MYSQL_HOST'],
+    user=os.environ['MYSQL_USER'],
+    password=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE'],
 )
+
 with connection:
     with connection.cursor() as cursor:
         #SQL
